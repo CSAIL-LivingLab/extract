7b2bf78724e01b982b28f9140f7dec93dc59cb7d
Pedro Cattori <pcattori@gmail.com>
2015-10-15 15:14:30 -0400

    Untracking build directory

--- build/lib/extract/prob.py
+++
@@ -1,7 +0,0 @@
-from scipy.stats import rv_discrete
-
-# probability utility functions
-
-def sample_pmf(vals, probs, size=1):
-    rv = rv_discrete(values=(vals, probs))
-    return rv.rvs(size=size)
